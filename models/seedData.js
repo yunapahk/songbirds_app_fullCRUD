3b253f08e44b224c3ece6480ca73c3e02fabb35c
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-06-26 03:25:19 -0700

    CSS styling added

---
+++ models/seedData.js
@@ -0,0 +1,9 @@
+const songbirds = [
+    {
+        name: "Eurasian Songbird",
+        specialty: "Loudest",
+        img: "https://i.imgur.com/VlYV7PZ.png"
+    }
+];   
+
+module.exports = songbirds
